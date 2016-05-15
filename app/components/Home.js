fb005949e09ea00d55c53679159b1b730eff5460
alanaut24 <alnhurt24@gmail.com>
2016-05-15 01:47:03 -0300

    3.0
    Updated app (players)

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,18 @@
 var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link
+var transparentBg = require('../styles').transparentBg;
 
-var Home = React.createClass({
-  render: function () {
-    return (
-      <div> Hello from Home! </div>
-    )
-  }
-});
+function Home () {
+  return (
+    <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+      <h1>Dribble Off</h1>
+      <p className='lead'>Who is the better Dribbbler?</p>
+      <Link to='/teamAway'>
+        <button type='button' className='btn btn-lg btn-success'>3..2..1</button>
+      </Link>
+    </div>
+  )
+}
 
 module.exports = Home;
--- app/config/routes.js
+++ app/config/routes.js
@@ -2,15 +2,18 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
+var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
-var hashHostory = ReactRouter.hashHostory;
 var Main = require('../components/Main');
-var Home = require('../components/Home');
+var Home = require("../components/Home");
+var PromptContainer = require('../containers/PromptContainer');
 
 var routes = (
-  <Router history={hashHostory}>
+  <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
+      <Route path='teamAway' header='Away' component={PromptContainer} />
+      <Route path='teamHome/:teamAway' header='Home' component={PromptContainer} />
     </Route>
   </Router>
 );
